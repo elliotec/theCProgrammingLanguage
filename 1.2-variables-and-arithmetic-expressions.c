f196f56c2470ff2ecca9db4650811b577d73994c
Mike <elliotecweb@gmail.com>
2018-04-07 17:56:56 -0600

    excersize 1-3

--- 1.2-variables-and-arithmetic-expressions.c
+++ 1.2-variables-and-arithmetic-expressions.c
@@ -11,6 +11,9 @@ main(){
   step = 10; // step size
 
   fahr = lower;
+
+  printf("Fahrenheit to Celsius:\n");
+
   while (fahr <= upper) {
     celsius = (5.0/9.0) * (fahr-32);
     printf("%3.0f %6.1f\n", fahr, celsius);
