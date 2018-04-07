539b4c6685432b3d6df02b18c3882bdc6f577cf7
Mike <elliotecweb@gmail.com>
2018-04-07 17:46:01 -0600

    1.2

---
+++ 1.2-variables-and-arithmetic-expressions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+ * for fahr = 0, 20, ..., 300; floating point version */
+main(){
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = -30; // lower limit of temperature table
+  upper = 200; // upper limit
+  step = 10; // step size
+
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr-32);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
+
